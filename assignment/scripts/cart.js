0def687318438ddffb26f78ffbe53487eec86f8a
Joe Wipperfurth <wipperfurth059@gmail.com>
2023-08-07 17:20:48 -0500

    Wrote out the functions for the first portion of the assignment though I didn't get to the stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,40 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem (item) {
+    basket.push("item");
+    return true;
+}
+
+function listItems (basket) {
+    for (let i = 0; i < basket.length; i++) 
+    console.log(basket(i))
+}
+//reference --------------------------------------
+// function displayArrayItems(array) {
+//     console.log(array);
+//   }
+
+
+// function displayArrayItems(array) {
+//     for (let i = 0; i < array.length; i++) {
+//       console.log(`Item ${i + 1}: ${array[i]}`);
+//     }
+//   }
+//reference --------------------------------------
+
+function empty (basket) {
+    basket.length = 0;
+}
+
+
+//reference --------------------------------------
+
+// function deleteArrayItems(array) {
+//     array.length = 0;
+//   }
+//reference --------------------------------------
 
 
 
